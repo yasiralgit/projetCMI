bd041078f1cb7bf71804b505956a37a6190bac88
Jade B <jadebolaty@gmail.com>
2024-04-04 22:00:16 +0200

    fonction qui traduit l'expression saisie en tableau de tableaux (avec autres fonctions intermédiaires et pour 4 entrées)

--- redOrdi.js
+++ redOrdi.js
@@ -5,21 +5,23 @@ class Logique {
     }
 
     VAR(){
-        return this.x
+        return [this.x]
     }
 
     ET(){
-        return this.x+"."+this.y
+        return (this.x).concat(this.y)
     }
 
     OU(){
-        return [this.x,"+",this.y]
+        return ([this.x]).concat(["+"].concat([this.y]))
     }
 
     NOT(){
-        return "!"+this.x
+        return [["!"],[this.x]]
     }
 }
+
+// sortie devrait être [["!A","B","C","D"],"+",["!A","!B","C","!D"],"+",["A","B","C","D"]]
 
 document.addEventListener("onclick", affichageTabK);
 
@@ -43,15 +45,66 @@ function affichageTabK() {
     }
 }
 
+function prod(expr){
+    for(let i=0;i<expr.length;i=i+1){
+        if (expr[i]=='+'){
+            return false;
+        }
+    }
+    return true;
+}
 
-test = "!A.B.C.D"
+function sectOU(expr){
+    let i = 0;
+    sousProdA = "";
+    sousProdB = "";
+    while (expr[i] != " "){
+        sousProdA += expr[i];
+        i += 1;
+    }
+    i += 3;
+    for (i;i<expr.length;i=i+1){
+        sousProdB += expr[i];
+    }
+    return [sousProdA,sousProdB]
+}
+
+function sectET(expr){
+    let i = 0;
+    sousProds = ["","","",""];
+    for (let j=0;j<expr.length;j=j+1){
+        if (expr[j]=="."){
+            i += 1;
+        }
+        else{
+            sousProds[i] += expr[j];
+        }
+    }
+    return sousProds
+}
+
+test = "!A.B.C.D + !A.!B.C.!D + A.B.C.D"
 function tabExpr(expr){
-    if (expr.length == 1){
+    if (prod(expr)){
+        sp = sectET(expr);
+        a = sp[0];
+        b = sp[1];
+        c = sp[2];
+        d = sp[3];
+        return new Logique(new Logique(new Logique(a).VAR(),new Logique(b).VAR()).ET(),new Logique(new Logique(c).VAR(),new Logique(d).VAR()).ET()).ET()
+    }
+    else{
+        console.log("on a pas tous les sous-produits");
+        sousProd = sectOU(expr);
+        console.log(sousProd[0]);
+        console.log(sousProd[1]);
+        return new Logique(tabExpr(sousProd[0]),tabExpr(sousProd[1])).OU()
     }
 }
 
-tabExpr(test);
+console.log(tabExpr(test));
+// sortie devrait être [["!A","B","C","D"],"+",["!A","!B","C","!D"],"+",["A","B","C","D"]]
 
 function tabK(tExpr){
-    tabK = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+    tabk = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 }
