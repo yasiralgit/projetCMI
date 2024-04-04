83d3558d820aa1ba16a3c03812c57fe8c6596cad
Jade B <jadebolaty@gmail.com>
2024-04-04 14:46:41 +0200

    meilleur affichage de la table de karnaugh avec debut de transformation en tab

--- redOrdi.js
+++ redOrdi.js
@@ -28,13 +28,30 @@ function recupExpr() {
 }
 
 function affichageTabK() {
-    $("#tableKar") = "";
+    tkar = "";
     const cb = document.getElementById("correK").checked;
     const exp = recupExpr();
     if (cb == true){
+        console.log(cb);
         if (exp != ''){
-            $("#tableKar").append("<thead><tr><th>AB\CD</th><th>00</th><th>01</th><th>11</th><th>10</th></tr></thead><tbody><tr><th>00</th><td>0</td><td>0</td><td>0</td><td>0</td></tr><tr><th>01</th><td>0</td><td>0</td><td>0</td><td>0</td></tr><tr><th>11</th><td>0</td><td>0</td><td>0</td><td>0</td></tr><tr><th>10</th><td>0</td><td>0</td><td>0</td><td>0</td></tr></tbody>");
-            console.log("<thead><tr><th>AB\CD</th><th>00</th><th>01</th><th>11</th><th>10</th></tr></thead><tbody><tr><th>00</th><td>0</td><td>0</td><td>0</td><td>0</td></tr><tr><th>01</th><td>0</td><td>0</td><td>0</td><td>0</td></tr><tr><th>11</th><td>0</td><td>0</td><td>0</td><td>0</td></tr><tr><th>10</th><td>0</td><td>0</td><td>0</td><td>0</td></tr></tbody>");
+            tableKar.innerHTML = "<table><thead><tr><th>AB\CD</th><th>00</th><th>01</th><th>11</th><th>10</th></tr></thead><tbody><tr><th>00</th><td>0</td><td>0</td><td>0</td><td>0</td></tr><tr><th>01</th><td>0</td><td>0</td><td>0</td><td>0</td></tr><tr><th>11</th><td>0</td><td>0</td><td>0</td><td>0</td></tr><tr><th>10</th><td>0</td><td>0</td><td>0</td><td>1</td></tr></tbody></table>"
+            console.log("procédé tableau de Karnaugh affiché");
         }
     }
+    else{
+        tableKar.innerHTML = ""
+    }
+}
+
+
+test = "!A.B.C.D"
+function tabExpr(expr){
+    if (expr.length == 1){
+    }
+}
+
+tabExpr(test);
+
+function tabK(tExpr){
+    tabK = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
 }
