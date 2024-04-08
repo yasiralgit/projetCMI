5fbb456a1d7918b1b3b6658cd329ad6c40926fcd
Jade B <jadebolaty@gmail.com>
2024-04-08 12:54:19 +0200

    tables vers expression blocs 1 2 et 4 et debut de 8

--- redOrdi.js
+++ redOrdi.js
@@ -142,10 +142,10 @@ function tabExpr2(expr){
         sp = sectET(expr);
         for (let i = 0;i<sp.length;i+=1){
             if (sp[i][0] == "!"){
-                sp[i] = new Logique2(sp[i]).NOT();
+                sp[i] = [new Logique2(sp[i]).NOT()];
             }
             else{
-                sp[i] = new Logique2(sp[i]).VAR();
+                sp[i] = [new Logique2(sp[i]).VAR()];
             }
         }
         //console.log(sp);
@@ -166,21 +166,21 @@ function tabExpr2(expr){
 // sortie devrait être ["0111","+","0010","+","1111"]
 
 function tabK(tExpr){
-    //console.log(tExpr);
+    console.log(tExpr);
     tabk = [["0000","0001","0011","0010"],
     ["0100","0101","0111","0110"],
     ["1100","1101","1111","1110"],
     ["1000","1001","1011","1010"]];
     tabKE = [["0","0","0","0"],["0","0","0","0"],["0","0","0","0"],["0","0","0","0"]];
     for (let i = 0;i<tExpr.length;i=i+2){
-        //console.log("boucle i", i);
+        console.log("boucle i", i);
         for (let j = 0;j<tabk.length;j=j+1){
-            //console.log("boucle j", j);
+            console.log("boucle j", j);
             for (let k = 0;k<tabk[j].length;k=k+1){
-                //console.log("tExpr et tabK = ", tExpr[i], tabk[j][k]);
+                console.log("tExpr et tabK = ", tExpr[i], tabk[j][k]);
                 if (tExpr[i] == tabk[j][k]){
                     tabKE[j][k] = "1";
-                    //console.log("normalement 1");
+                    console.log("normalement 1");
                 }
             } 
         }
@@ -208,7 +208,7 @@ function tabKStr(tKar){
 }
 
 //prototype de yasir
-function kar2exp() {
+function kar2exp(tbk) {
     var marTrans = [
     ['0000','0001','0011','0010'],
     ['0100','0101','0111','0110'],
@@ -218,7 +218,7 @@ function kar2exp() {
     var marTrad = [
         ['!A.!B.!C.!D','!A.!B.!C.D','!A.!B.C.D','!A.!B.C.!D'],
         ['!A.B.!C.!D','!A.B.!C.D','!A.B.C.D','!A.B.C.!D'],
-        ['A.B.!C.!D','A.B.!C.D','A.B.C.D','A.B.C.D'],
+        ['A.B.!C.!D','A.B.!C.D','A.B.C.D','A.B.C.!D'],
         ['A.!B.!C.!D','A.!B.!C.D','A.!B.C.D','A.!B.C.!D']
         ];
     var marTEST = [
@@ -227,36 +227,110 @@ function kar2exp() {
     [0,0,1,0],
     [0,0,0,0]
     ];
-    var cpt = 0 ; var pos = [];
-    for (let i = 0 ; i<marTEST.length ; i++) {
-        for (let j = 0 ;j < marTEST[i].length ; j++) {
-            if (marTEST[i][j] == 1) {
+    var cpt = 0 ; var pos = []; var coor = []; var bloc1 = []; var bloc2 = []; var bloc4 = []; var verifVoisins;
+    for (let i = 0 ; i<tbk.length ; i++) {
+        for (let j = 0 ;j < tbk[i].length ; j++) {
+            if (tbk[i][j] == 1) {
                 cpt ++ 
                 pos.push(marTrans[i][j])
+                coor.push([i,j]);
             }
 
         }
     }
-    console.log(pos)
+    console.log(pos);
+    console.log(coor);
     if (pos.length == 1) {
+        console.log("1 seul 1");
         return pos[0]
     }
-    if (pos.length == 2){
-        for (let k = 0 ; k < 4 ; k++){
-            if (pos[0][k] != pos[1][k]) {
-                //ds
-            } 
+    if (pos.length == 16) {
+        console.log("seize 1");
+        return "1";
+    }
+    for (let i = 0;i<coor.length;i++){
+        verifVoisins = false;
+        for (let j = i+1;j<coor.length;j++){
+            console.log(coor[i][0]);
+            if ((coor[j][0] == coor[i][0])&&(((coor[j][1]-coor[i][1])**2)==1)||(coor[j][1] == coor[i][1])&&(((coor[j][0]-coor[i][0])**2)==1)){
+                console.log("premier if");
+                bloc2.push([coor[i],coor[j]]);
+                verifVoisins = true;
+            }
+            if ((coor[j][0] == coor[i][0])&&(((coor[j][1]-coor[i][1])**2)==9)||(coor[j][1] == coor[i][1])&&(((coor[j][0]-coor[i][0])**2)==9)){
+                console.log("2eme if");
+                bloc2.push([coor[i],coor[j]]);
+                verifVoisins = true;
+            }
+            if(coor[i][1]==3){
+                for (elt of bloc2){
+                    if (elt.includes(coor[i])){ //bloc2 = [[[i,j],[i,j]],[[i,j],[i,j]]]
+                        verifVoisins = true;
+                    }
+                }
+            }
+
+        }
+        if(i==coor.length-1){
+            for (elt of bloc2){
+            if (elt.includes(coor[i])){ //bloc2 = [[[i,j],[i,j]],[[i,j],[i,j]]]
+                verifVoisins = true;
+            }
+            }
+        }
+        if (!verifVoisins){
+            console.log("tt seul");
+            bloc1.push(coor[i]);
         }
     }
-    
+    for (let i = 0;i<bloc2.length;i++){
+        verifVoisins = false;
+        for (let j = i+1;j<bloc2.length;j++){
+            if(!((bloc2[j].includes(bloc2[i][0])) || (bloc2[j].includes(bloc2[i][1])))){
+                if (bloc2[i][0][0] == bloc2[i][1][0] == bloc2[j][0][0] == bloc2[j][1][0]){
+                    bloc4.push([bloc2[i],bloc2[j]]);
+                    verifVoisins = true;
+                }
+                if (bloc2[i][0][1] == bloc2[i][1][1] == bloc2[j][0][1] == bloc2[j][1][1]){
+                    bloc4.push([bloc2[i],bloc2[j]]);
+                    verifVoisins = true;
+                }
+                if (((bloc2[i][0][0] == bloc2[j][0][0])&&(((bloc2[i][0][1]-bloc2[j][0][1])**2)==1))||((bloc2[i][0][1] == bloc2[j][0][1])&&(((bloc2[i][0][0]-bloc2[j][0][0])**2)==1))){
+                    if(((bloc2[i][1][0] == bloc2[j][1][0])&&(((bloc2[i][1][1]-bloc2[j][1][1])**2)==1))||((bloc2[i][1][1] == bloc2[j][1][1])&&(((bloc2[i][1][0]-bloc2[j][1][0])**2)==1))){
+                        bloc4.push([bloc2[i],bloc2[j]]);
+                        verifVoisins = true;
+                    }
+                }
+                if (((bloc2[i][0][0] == bloc2[j][0][0])&&(((bloc2[i][0][1]-bloc2[j][0][1])**2)==9))||((bloc2[i][0][1] == bloc2[j][0][1])&&(((bloc2[i][0][0]-bloc2[j][0][0])**2)==9))){
+                    if(((bloc2[i][1][0] == bloc2[j][1][0])&&(((bloc2[i][1][1]-bloc2[j][1][1])**2)==9))||((bloc2[i][1][1] == bloc2[j][1][1])&&(((bloc2[i][1][0]-bloc2[j][1][0])**2)==9))){
+                        bloc4.push([bloc2[i],bloc2[j]]);
+                        verifVoisins = true;
+                    }
+                }
+            }
+        }
+    } //bloc4 = [[[[i,j],[i,j]],[[i,j],[i,j]]],[[[i,j],[i,j]],[[i,j],[i,j]]]]
+    for (let i = 0;i<bloc4.length;i++){
+        verifVoisins = false;
+        for (let j = i+1;j<bloc4.length;j++){
+            if(!((bloc4[j][0].includes(bloc4[i][0])) || (bloc4[j][0].includes(bloc4[i][1])) || (bloc4[j][1].includes(bloc4[i][0])) || (bloc4[j][1].includes(bloc4[i][1])))){
+                console.log("pas confondus");
+            }
+        }
+    }
+    console.log("bloc 4", bloc4);
+    console.log("bloc 2 ", bloc2);
+    console.log("bloc 1", bloc1);
    }
 
 
 function test2(){
     //console.log("logique 1",tabExpr(recupExpr()));
     //console.log("logique 2",tabExpr2(recupExpr()));
+    console.log(tabExpr2(recupExpr()))
     tabkar = tabK(tabExpr2(recupExpr()));
-    //console.log(tabkar);
+    console.log(tabkar);
     //console.log(tabKStr(tabkar));
-    affichageTabK(tabKStr(tabkar))
+    affichageTabK(tabKStr(tabkar));
+    kar2exp(tabkar)
 }
