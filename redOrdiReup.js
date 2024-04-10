ce5e099683267e7700e326470b5a1bac8507f82a
megasierax <lovesickjade@gmail.com>
2024-04-10 10:34:54 +0200

    création du graphe/liste d'adjacence sous forme de dictionnaire

--- redOrdiReup.js
+++ redOrdiReup.js
@@ -7,49 +7,31 @@ var testKar = [
 
 
 function graphe(tabKar){
-    var g = [];
+    var g = {}; var aTraiter = {};
     for (let i = 0;i<tabKar.length;i++){
         for (let j = 0;j<tabKar[i].length;j++){
-            g.push([])
             if (tabKar[i][j] == 1){
+                g[i.toString()+j.toString()] = [];
+                aTraiter[i.toString()+j.toString()] = false;
                 var left = j-1; var right = j+1; var up = i-1; var down = i+1;
                 if(left == -1){left = 3;} if(right == 4){right = 0;} if(up == -1){up = 3;} if(down == 4){down = 0;}
                 //console.log(tabKar[i][left])
-                if (tabKar[i][left] == 1){g[i*4+j].push(i.toString()+left.toString());}
+                if (tabKar[i][left] == 1){g[i.toString()+j.toString()].push(i.toString()+left.toString());}
                 //console.log(tabKar[i][right])
-                if (tabKar[i][right] == 1){g[i*4+j].push(i.toString()+right.toString());}
+                if (tabKar[i][right] == 1){g[i.toString()+j.toString()].push(i.toString()+right.toString());}
                 //console.log(up)
                 //console.log(tabKar[up][j])
-                if (tabKar[up][j] == 1){g[i*4+j].push(up.toString()+j.toString());}
-                if (tabKar[down][j] == 1){g[i*4+j].push(down.toString()+j.toString());}
-            }
-            else{
-                g[i*4+j].push(null)
+                if (tabKar[up][j] == 1){g[i.toString()+j.toString()].push(up.toString()+j.toString());}
+                if (tabKar[down][j] == 1){g[i.toString()+j.toString()].push(down.toString()+j.toString());}
             }
         }
     }
-    return g;
+    return [g,aTraiter]
 }
 
-function blocs(tabKar,g){
-    var compBloc = [];
-    var combAssoc = [];
-    for (let i = 0;i<g.length;i++){
-        if (g[i]!=none){
-            var j = 0; var isInBloc = true; var count=1;
-            compBloc.push([])
-            while(isInBloc){
-                compBloc[i].push(g[i][j]);
-                for (let k=0;k<g[g[i][j][0]*3+g[i][j][1]].length;k++){
-                    var l = j+1;
-                    for (let l = j+1;l<g[i].length;l++){
-                        if (g[[i][l][0]*3+g[i][l][1]].includes(g[g[i][j][0]*3+g[i][j][1]][k])){count += 1;}
-                        l = l+1;
-                    }
-                }
-            }
-        }
-    }
+
+function composantesBlocs(g,aTraiter){
+    
 }
 
 
