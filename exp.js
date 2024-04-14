f489bba2b4508b0a742a652ad67d17b1d3fdcc6d
yasiralgit <ml.yasir.alyoussfi@gmail.com>
2024-04-14 11:55:48 +0200

    Push du travail effectué en salle de tp pout pour l'utiliser à domicile

---
+++ exp.js
@@ -0,0 +1,25 @@
+class Logique {
+    constructor(x,y=""){
+        this.x = x
+        this.y = y
+    }
+
+    VAR(){
+        return this.x
+    }
+
+    ET(){
+        return this.x+"."+this.y
+    }
+
+    OU(){
+        return [this.x,"+",this.y]
+    }
+
+    NOT(){
+        return "!"+this.x
+    }
+}
+
+const test = new Logique('A')
+console.log(test)
