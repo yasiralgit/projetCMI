c5686c041b364b9208a4d9715ba55eb99a155872
megasierax <lovesickjade@gmail.com>
2024-04-26 15:13:09 +0200

    fichier obsolète

--- redOrdiReup.js
+++ redOrdiReup.js
@@ -8,7 +8,7 @@ class Pile {
     }
 
     depiler(){
-        if (!((this.p)==[])){
+        if (!(this.estVide())){
             return (this.p).pop()
         }
     }
@@ -16,34 +16,46 @@ class Pile {
     estVide(){
         return ((this.p).length == 0)
     }
+
+    appartient(e){
+        var pileInt; var isInP = false; var elt;
+        pileInt = new Pile();
+        while((!(this.estVide()))&&(!isInP)){
+            elt = this.depiler(); pileInt.empiler(elt);
+            if (elt==e){
+                isInP = true;
+            }
+        }
+        while(!(pileInt.estVide())){
+            this.empiler(pileInt.depiler());
+        }
+        return isInP
+    }
+
+    len(){
+        var pileInt; var cpt=0; var elt;
+        pileInt = new Pile();
+        while((!(this.estVide()))){
+            elt = this.depiler(); pileInt.empiler(elt); cpt += 1;
+        }
+        while(!(pileInt.estVide())){
+            this.empiler(pileInt.depiler());
+        }
+        return cpt
+    }
 }
 
 
 
-var testKar = [
-    [1,1,1,1],
-    [1,1,1,1],
-    [0,0,0,0],
-    [0,0,0,0]
+var testKar = [ //!A!B + D + !A.C + AB + A.!C (d'après l'algo)
+    [1,1,1,1],  //D + A.!C + B.D + !A!B (vraie réponse)
+    [1,1,0,1],  //pb 6 7
+    [0,0,1,0],  //normalement rajouter un while et une verif sur le pb 7
+    [1,0,0,0]
 ];
 
 
-function appartient(e,b){
-    var pileInt; var isInB = false; var elt;
-    pileInt = new Pile();
-    while((!(b.estVide()))&&(!isInB)){
-        elt = b.depiler(); pileInt.empiler(elt);
-        if (elt==e){
-            isInB = true;
-        }
-    }
-    while(pileInt.estVide()!=true){
-        b.empiler(pileInt.depiler());
-    }
-    return isInB
-}
-
-//tests fct appartient
+//tests fct appartient et len
 /**pileTest = new Pile();
 console.log(pileTest.estVide());
 console.log(pileTest);
@@ -52,7 +64,8 @@ pileTest.empiler('a');
 pileTest.empiler('d');
 pileTest.empiler('e');
 console.log(pileTest);
-console.log(appartient('a',pileTest));*/
+console.log(pileTest.appartient('y'));
+console.log(pileTest.len());*/
 
 
 function graphe(tabKar){
@@ -70,7 +83,7 @@ function graphe(tabKar){
                 if (tabKar[i][right] == 1){g[i.toString()+j.toString()].push(i.toString()+right.toString());}
                 //console.log(up)
                 //console.log(tabKar[up][j])
-                if (tabKar[up][j] == 1){g[i.toString()+j.toString()].push(up.toString()+j.toString());}
+            if (tabKar[up][j] == 1){g[i.toString()+j.toString()].push(up.toString()+j.toString());}
                 if (tabKar[down][j] == 1){g[i.toString()+j.toString()].push(down.toString()+j.toString());}
             }
         }
@@ -91,47 +104,186 @@ console.log(pTest);*/
 
 
 function lBlocs(g,aTraiter){// voir explications explicites sur le rapport final (avec d'éventuels schémas)
-    var listeBlocs = {}; var pileBloc; var include;
+    var listeBlocs = {}; var pileBloc; var include_i; var include_j; var nbV; var tousVus=false; var jamaisVu;
     for (som in g){ //itération sur les sommets de la liste d'adjacence
         if (!aTraiter[som]){ //on entre seulement si le sommet n'a pas déjà été traité
             pileBloc = new Pile(); //on crée la pile nécessaire pour stocker les éléments du bloc de puissance de 2
             pileBloc.empiler(som); aTraiter[som] = true; //on empile le sommet en cours de traitement, et on le passe à vrai
-            if(g[som].length > 0){pileBloc.empiler(g[som][0]); aTraiter[g[som][0]] = true;}//si il a au moins un voisin on l'empile, et éventuellement on entre dans les boucles, sinon on va directement à la ligne après la première boucle for
-            for (let j = 0;j<g[som].length;j++){//itération sur tous les voisins du sommet courant
+            if(g[som].length == 1){pileBloc.empiler(g[som][0]); aTraiter[g[som][0]] = true;}//si il a au moins un voisin on l'empile, et éventuellement on entre dans les boucles, sinon on va directement à la ligne après la première boucle for
+            for (let j = 0;j<g[som].length-1;j++){//itération sur tous les voisins du sommet courant
+                include_j = false; jamaisVu = true
                 for (let i=j+1;i<g[som].length;i++){//itération sur les voisins du sommet courant autre que celui précédent
-                    if(!(appartient(g[som][i],pileBloc))){pileBloc.empiler(g[som][i]);}//en lien avec ligne 93, on l'empile avec la condition qu'il permette un agrandissement à un bloc d'une taille de puissance de 2, et qu'il n'y soit pas déjà pour éviter les doublons (cas d'un bloc de 8 par ex)
-                    include = false;
+                    if(!(pileBloc.appartient(g[som][i]))){pileBloc.empiler(g[som][i]);include_i = false;}//en lien avec ligne 93, on l'empile avec la condition qu'il permette un agrandissement à un bloc d'une taille de puissance de 2, et qu'il n'y soit pas déjà pour éviter les doublons (cas d'un bloc de 8 par ex), le include false ref a projet pb 3
                     for (autreSom in g){ //on itère sur tout les autres sommets du graphe
                         if (autreSom != som){ //on exclut le sommet courant som
                             if (g[autreSom].includes(g[som][j])&&g[autreSom].includes(g[som][i])){ //on vérifie si autreSom est bien voisin avec deux voisins communs au sommet courant som
                                 aTraiter[g[som][i]] = true;
-                                if(!(appartient(autreSom,pileBloc))){//pour éviter les doublons
+                                if(!(pileBloc.appartient(autreSom))){//pour éviter les doublons
                                     pileBloc.empiler(autreSom); aTraiter[autreSom] = true;}
-                                if(!(appartient(g[som][j],pileBloc))){//pour éviter les doublons
+                                if(!(pileBloc.appartient(g[som][j]))){//pour éviter les doublons
                                     pileBloc.empiler(g[som][j]); aTraiter[g[som][j]] = true;}
-                                include = true;//pour le garder dans le bloc puisqu'il répond à la condition
+                                include_i = true;include_j = true;//pour le garder dans le bloc puisqu'il répond à la condition
                             }
                         }
                     }
-                    if (!(include)){pileBloc.depiler();}//si on a trouvé aucun autre voisin permettant un bloc d'une taille de puissance de 2, on l'enlève du bloc, il n'en fait pas parti
+                    if (!(include_i)){pileBloc.depiler();}//si on a trouvé aucun autre voisin permettant un bloc d'une taille de puissance de 2, on l'enlève du bloc, il n'en fait pas parti
+                }
+                if (!(include_j)){
+                    s = pileBloc.depiler();
+                    if (s==g[som][j]){
+                        for (b in listeBlocs){
+                            if(b==s||listeBlocs[b].appartient(s)){
+                                jamaisVu = false;
+                            }
+                        }
+                        if(jamaisVu){aTraiter[s] = false;}}
+                    else{
+                        pileBloc.empiler(s);
+                    }
                 }
             }
-            listeBlocs[som] = pileBloc;//le bloc final est crée, on peut le rajouter au dictionnaire et ne plus jamais le retoucher (hormis blocs de 8 et +)
+            var existeBloc;
+            if(g[som].length>0&&pileBloc.len()==1){//pb pour un certain cas cf photo 20 avril #1
+                aTraiter[pileBloc.depiler()] = false;
+            }
+            else if(pileBloc.len()==6){
+                while(pileBloc.len()!=4){
+                    existeBloc = false;
+                    s = pileBloc.depiler();
+                    for (b in listeBlocs){
+                        if(b==s||listeBlocs[b].appartient(s)){existeBloc = true;}
+                    }
+                    if(!(existeBloc)){aTraiter[s] = false;}
+                    //projet pb 4 ou 5 jsp (on a deux fois la mm pile)
+                }
+            }
+            if(pileBloc.len()>=7){
+                nbV = {};
+                for(let k=0;k<g[som].length;k++){
+                    for(let l=0;l<g[g[som][k]].length;l++){
+                        for(let m=l+1;m<g[g[som][k]].length;m++){
+                            for (autreSom in g){
+                                if (autreSom!=g[som][k]&&pileBloc.appartient(g[g[som][k]][l])&&pileBloc.appartient(g[g[som][k]][m])){
+                                    if(g[autreSom].includes(g[g[som][k]][l])&&g[autreSom].includes(g[g[som][k]][m])){
+                                        if (autreSom in nbV){
+                                            nbV[autreSom] += 1;
+                                        }
+                                        else{
+                                            nbV[autreSom] = 1;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                longPrev = pileBloc.len();
+                for(v in nbV){
+                    if (nbV[v]>=3&&(!(pileBloc.appartient(v)))){
+                        pileBloc.empiler(v); aTraiter[v] = true;
+                    }
+                }
+                if (longPrev==pileBloc.len()){
+                    while(pileBloc.len()!=4){
+                        existeBloc = false;
+                        s = pileBloc.depiler();
+                        for (b in listeBlocs){
+                            if(b==s||listeBlocs[b].appartient(s)){existeBloc = true;}
+                        }
+                        if(!(existeBloc)){aTraiter[s] = false;}
+                        //projet pb 4 ou 5 jsp (on a deux fois la mm pile) avec bloc de 7
+                    }
+                }
+            }
+            if(pileBloc.len()==15){
+                for(s in aTraiter){
+                    if (!(aTraiter[s])){
+                        pileBloc.empiler(s); aTraiter[s] = true;
+                    }
+                }
+            }
+            //console.log("ici", Math.log2(pileBloc.len()))
+            if(((Math.log2(pileBloc.len())*2)%2)!=0){ //on lui a laissé sa chance dans la boucle précédente, si c'est tjrs pas une puissance de deux, on le supprime
+                //console.log("hello", pileBloc.len());
+                while(!(pileBloc.estVide())){
+                    aTraiter[pileBloc.depiler()] = false;//projet pb 2 mais avec bloc de 10
+                }
+            }
+            if ((!(pileBloc.estVide()))&&(!(aTraiter[som]==false))){listeBlocs[som] = pileBloc;}//le bloc final est crée, on peut le rajouter au dictionnaire et ne plus jamais le retoucher (hormis blocs de 8 et +)
+            //ici enfait
         }
     }
     /**pb à régler : on travaille avec des hypercubes donc le nombre de voisins max possible limite le parcours de tous les sommets permettant de construire un bloc d'une taille de puissance de 2 (y'en a qui se situent trop loin par rapport au sommet courant)
      * -blocs de 8: les clés des deux blocs sortant dans listeBlocs s'excluent mutuellement, donc faut les fusionner (piles de 7 elts au lieu de 8)
-     * -blocs de 16: mm pb sauf que au lieu de manquer que 1 élément, il lui en manque 5 et de la même manière, mutuellement avec la deuxième pile
-    */
+     * -blocs de 16: mm pb sauf que au lieu de manquer que 1 élément, il lui en manque 5 et de la même manière, mutuellement avec la deuxième pile*/
+    /**tousVus = true;
+    for (som in aTraiter){
+    if (aTraiter[som]==false){tousVus = false;}
+    }*/
+
 
     console.log(listeBlocs); //on retourne le dico des blocs selon une origine pour pouvoir ensuite passer à la traduction
+    //console.log(aTraiter);
+    return listeBlocs
 }
+
+function sectET(expr){
+    let i = 0;
+    sousProds = ["","","",""];
+    for (let j=0;j<expr.length;j=j+1){
+        if (expr[j]=="."){
+            i += 1;
+        }
+        else{
+            sousProds[i] += expr[j];
+        }
+    }
+    return sousProds
+}
+
+function trad(listeBlocs){
+    var expr = ''; var prod; var long; var lProds; var memeEntree;
+    var valTrad = [
+        ['!A.!B.!C.!D','!A.!B.!C.D','!A.!B.C.D','!A.!B.C.!D'],
+        ['!A.B.!C.!D','!A.B.!C.D','!A.B.C.D','!A.B.C.!D'],
+        ['A.B.!C.!D','A.B.!C.D','A.B.C.D','A.B.C.!D'],
+        ['A.!B.!C.!D','A.!B.!C.D','A.!B.C.D','A.!B.C.!D']
+    ];
+    for (b in listeBlocs){
+        prod = []; long = listeBlocs[b].len(); lProds = [];
+        if (expr!=''){
+            expr += ' + ';
+        }
+        for (let i = 0;i<long;i++){
+            coor = listeBlocs[b].depiler();
+            console.log(coor);
+            prod.push(valTrad[coor[0]][coor[1]]);
+        }
+        for (let j = 0;j<prod.length;j++){
+            lProds.push(sectET(prod[j]));
+        }
+        for (let k = 0;k<lProds[0].length;k++){
+            memeEntree = true;
+            for (let l = 1;l<lProds.length;l++){
+                if (lProds[0][k]!=lProds[l][k]){
+                    memeEntree = false;
+                }
+            }
+            if (memeEntree){
+                expr += lProds[0][k];
+            }
+        }
+    }
+    return expr
+}
+
+
 
 gTest = graphe(testKar)[0];
 aTraiterTest = graphe(testKar)[1];
 console.log(gTest);console.log(aTraiterTest);
-lBlocs(gTest,aTraiterTest);
-
+lBlocsTest = lBlocs(gTest,aTraiterTest);
+console.log(trad(lBlocsTest));
 
 
 
